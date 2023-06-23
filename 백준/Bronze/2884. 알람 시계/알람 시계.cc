01de1494227475718c7003e605dfa8734c4ceee3
YeWon Seo <syw1236@naver.com>
2023-06-23 21:16:55 +0900

    [Bronze III] Title: 알람 시계, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2884. 알람 시계/알람 시계.cc
+++ 백준/Bronze/2884. 알람 시계/알람 시계.cc
@@ -5,12 +5,12 @@ int main() {
 	int h, m;
 	cin >> h >> m;
 	if (m < 45) {
-		m = 60 - 45 + m;
+		m += 15;
 		h--;
 		if (h < 0)
 			h = 23;
 	}
 	else
-		m = m - 45;
+		m -= 45;
 	cout << h << " " << m;
 }

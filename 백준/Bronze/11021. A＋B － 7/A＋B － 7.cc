27c5a6a5001f9b2aaa93b085a7dcc918f17bcc5d
YeWon Seo <syw1236@naver.com>
2023-06-24 01:02:41 +0900

    [Bronze V] Title: A+B - 7, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int N, a, b;
+	cin >> N;
+	for (int i = 0; i < N; i++) {
+		cin >> a >> b;
+		cout << "Case #" << i + 1 << ": " << a + b<<"\n";
+	}
+}

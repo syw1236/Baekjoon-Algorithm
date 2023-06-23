6f133030ae5cf474bdda9ff7af6774907be1b63f
YeWon Seo <syw1236@naver.com>
2023-06-24 00:26:52 +0900

    [Bronze V] Title: 영수증, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25304. 영수증/영수증.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int X, N,a,b,sum=0;
+	cin >> X;
+	cin >> N;
+	for (int i = 0; i < N; i++) {
+		cin >> a >> b;
+		sum += a * b;
+	}
+	if (X == sum)
+		cout << "Yes";
+	else
+		cout << "No";
+}

87084a0bc9920ecf94fb35ee66f03843e4b5dd0a
YeWon Seo <syw1236@naver.com>
2023-07-01 03:05:19 +0900

    [Bronze III] Title: 윷놀이, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2490. 윷놀이/윷놀이.cc
+++ 백준/Bronze/2490. 윷놀이/윷놀이.cc
@@ -1,24 +1,17 @@
 #include <iostream>
-#include <algorithm>
 using namespace std;
 
 int main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
-	int a;
+	int result, input;
+	string res = "DCBAE";
 	for (int i = 0; i < 3; i++) {
-		int zero = 0, one = 0;
+		result = 0;
 		for (int j = 0; j < 4; j++) {
-			cin >> a;
-			if (a == 0) zero++;
-			else one++;
-				
+			cin >> input;
+			result += input;
 		}
-		if (zero == 1 && one == 3)cout << "A";
-		else if (zero == 2 && one == 2)cout << "B";
-		else if (zero == 3 && one == 1)cout << "C";
-		else if (zero == 4)cout << "D";
-		else cout << "E";
-		cout << "\n";
+		cout << res[result] << "\n";
 	}
 }

8cb0b620fde0b755712fdd653ffada59d507be80
YeWon Seo <syw1236@naver.com>
2023-07-01 16:36:35 +0900

    [Bronze III] Title: 별 찍기 - 8, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2445. 별 찍기 － 8/별 찍기 － 8.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int n;
+    cin >> n;
+    for (int i = n-1; i >= 1; i--) {//4
+        for (int j = 1; j <= n-i; j++)
+            cout << "*";
+        for (int j = 1; j <= 2 * i; j++)
+            cout << " ";
+        for (int j = 1; j <= n - i; j++)
+            cout << "*";
+        cout << "\n";    
+    }
+    for (int i = 1; i <= 2 * n; i++)
+        cout << "*";
+    cout << "\n";
+    for (int i = 1; i<=n-1; i++) {
+        for (int j = 1; j <= n-i; j++)
+            cout << "*";
+        for (int j = 1; j <= 2 * i; j++)
+            cout << " ";
+        for (int j = 1; j <= n - i; j++)
+            cout << "*";
+        cout << "\n";
+    }
+
+}

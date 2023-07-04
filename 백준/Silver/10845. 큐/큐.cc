23a424f723b36ba1fdaf0a12c04d7331e6493672
YeWon Seo <syw1236@naver.com>
2023-07-04 21:33:29 +0900

    [Silver IV] Title: 큐, Time: 0 ms, Memory: 2156 KB -BaekjoonHub

---
+++ 백준/Silver/10845. 큐/큐.cc
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	int n;
+	queue<int> Q;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		string s;
+		cin >> s;
+		if (s == "push") {
+			int add;
+			cin >> add;
+			Q.push(add);
+		}
+		else if (s == "pop") {
+			if (!Q.empty()) {
+				cout << Q.front() << '\n';
+				Q.pop();
+			}
+			else
+				cout << -1 << '\n';
+		}
+		else if (s == "size")
+			cout << Q.size() << '\n';
+		else if (s == "empty") 
+			cout << (int)Q.empty() << '\n';
+		else if (s == "front") {
+			if (!Q.empty())
+				cout << Q.front() << '\n';
+			else
+				cout << -1 << '\n';
+		}
+		else if (s == "back") {
+			if (!Q.empty())
+				cout << Q.back() << '\n';
+			else cout << -1 << '\n';
+		}
+	}
+}

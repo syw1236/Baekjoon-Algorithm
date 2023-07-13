ab730b71a718bf435f2b0d0a31297ef76f0e5f00
YeWon Seo <syw1236@naver.com>
2023-07-13 14:25:53 +0900

    [Silver I] Title: IOIOI, Time: 12 ms, Memory: 3680 KB -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.cc
+++ 백준/Silver/5525. IOIOI/IOIOI.cc
@@ -10,16 +10,18 @@ int main() {
 	int i = 0;
 	cin >> n >> m >> str;
 	for (int idx = 0; idx < m; idx++) {
-		if (str[idx] == 'O')continue; 
-		//처음 시작 'I'
 		int k = 0;
-		int i = idx;
-		while (str[i + 1] == 'O' && str[i + 2] == 'I') {//OI가 계속 이어나올 경우 k 증가
+		if (str[idx] == 'O')continue; 
+		//처음 시작 'I'	
+		while (str[idx + 1] == 'O' && str[idx + 2] == 'I') {//OI가 계속 이어나올 경우 k 증가
 			k++; 
-			if (k == n) //k가 해당 n과 같을 경우 카운트 1증가
+			if (k == n) { //k가 해당 n과 같을 경우 카운트 1증가
 				cnt++;
-			i += 2; //해당 인덱스 증가
+				k--; //다음을 확인하기 위해 --해줌
+			}
+			idx += 2; //해당 인덱스 증가
 		}
+		
 	}
 	cout << cnt << '\n';
 }

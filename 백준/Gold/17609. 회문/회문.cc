5e4d84694ab4facfec382e1d79a9ca29120e2e88
YeWon Seo <syw1236@naver.com>
2023-07-13 11:19:58 +0900

    [Gold V] Title: 회문, Time: 20 ms, Memory: 2500 KB -BaekjoonHub

--- 백준/Gold/17609. 회문/회문.cc
+++ 백준/Gold/17609. 회문/회문.cc
@@ -6,7 +6,7 @@ string str;
 int check_Palindrome(int left, int right,bool check){
 	while (left < right) {
 		if (str[left] != str[right]) {//비교하는 문자가 같지 않을 때
-			if (check) {
+			if (check) {//필요없는 확인을 줄임
 				if (check_Palindrome(left + 1, right,false) == 0 || check_Palindrome(left, right - 1,false) == 0) {//그 앞의 문자와 각각 비교
 					return 1;
 				}
